cb25e242e068b63fbcd2d92bf5f718da5a664588
unknown <gayatrimathe45@gmail.com>
2025-10-04 11:52:20 +0530

    ifex

---
+++ ifex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float n;
+    printf("enter a number");
+    scanf("%f",&n);
+if(n>0)
+    printf("+ve number");
+else
+    printf("-ve number");
+return 0;
+}       
