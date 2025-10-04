4ed981926185b34680aa5b74c9c54d6321668e8f
unknown <gayatrimathe45@gmail.com>
2025-10-04 11:41:26 +0530

    swap3

---
+++ swap3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    printf("\n a=%d",a);
+    printf("\n b=%d",b);
+    return 0;
+}
